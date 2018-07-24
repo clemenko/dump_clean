4d15811f33ff492ceccfbf3082645abb2548e314
Andy Clemenko <clemenko@gmail.com>
2018-07-24 15:52:16 -0400

    updating dump_clean

--- dump_clean.sh
+++ dump_clean.sh
@@ -4,30 +4,62 @@
 ###################################
 set -e
 password=Pa22word
+URL=ucp.dockr.life
 data_dir=/Users/clemenko/Dropbox/docker/dump_clean
 
-#get dump
-token=$(curl -sk -d '{"username":"admin","password":"'$password'"}' https://ucp.dockr.life/auth/login | jq -r .auth_token) > /dev/null 2>&1
+
+######  NO MOAR EDITS #######
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+
+echo "------------------------------------------------------------------"
+echo " Welcome to Docker Support Dump Data Scrubber."
+echo "------------------------------------------------------------------"
+
+
+echo -n " Getting Support Dump "
+token=$(curl -sk -d '{"username":"admin","password":"'$password'"}' https://$URL/auth/login | jq -r .auth_token)
 curl -kX POST 'https://ucp.dockr.life/api/support'  -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9' -H "Authorization: Bearer $token" --compressed > $data_dir/original-support.tgz
+echo "$GREEN" " [ok]" "$NORMAL"
+
 
 #no need to copy the tar since untar makes a copy.
-#untar
+echo -n " Untar-ing "
 mkdir dsinfo
 tar -zxf $data_dir/original-support.tgz -C dsinfo
 cd dsinfo
+echo "$GREEN" "[ok]" "$NORMAL"
 
-#get hostname and scrub them.
-host_num=0
-rm -f conversion.txt
+
+echo -n " Generating Swap list "
+host_num=1
+echo "HOST NEW_HOST IP NEW_IP" > $data_dir/conversion.txt
 for hostname in $(cat ucp-nodes.txt |jq -r .[].Description.Hostname); do
-  echo "$hostname ==> host_$host_num"
-  grep -lRi $hostname . | xargs sed -i '' "s/$hostname/host_$host_num/g"
-  echo "$hostname host_$host_num" >> conversion.txt
+  orig_ip=$(cat ucp-nodes.txt |jq -r '.[] | select(.Description.Hostname=="'$hostname'") | .Status.Addr')
+  echo "$hostname host_$host_num $orig_ip 0.0.0.$host_num" >> $data_dir/conversion.txt
   host_num=$((host_num+1))
-  mv $hostname* host_$host_num
 done
+echo "$GREEN" "[ok]" "$NORMAL"
 
-#sed - with change log
+echo -n " Scrubbing hostnames "
+for hostname in $(cat $data_dir/conversion.txt | grep -v HOST | awk '{print $1}'); do
+  new_host=$(cat $data_dir/conversion.txt |grep $hostname | awk '{print $2}')
+  grep -lRi $hostname . | xargs sed -i '' "s/$hostname/$new_host/g"
+  mv $hostname* $new_host
+done
+echo "$GREEN" "[ok]" "$NORMAL"
 
+echo -n " Scrubbing IPs "
+for ip in $(cat $data_dir/conversion.txt | grep -v HOST | awk '{print $3}'); do
+  new_ip=$(cat $data_dir/conversion.txt |grep $ip | awk '{print $4}')
+  grep -lRi $ip . | xargs sed -i '' "s/$ip/$new_ip/g"
+done
+echo "$GREEN" "[ok]" "$NORMAL"
 
-#repackage
+echo -n " Scrubbing Domains "
+echo "$GREEN" "[ok]" "$NORMAL"
+
+echo -n " Repackaging "
+tar -zcvf $data_dir/cleaned_support_dump.tgz .  > /dev/null 2>&1
+echo "$GREEN" "[ok]" "$NORMAL"
--- readme.md
+++ readme.md
@@ -1,11 +1,21 @@
-#My back-of-the-napkin thoughts:
+# My back-of-the-napkin thoughts:
 
-##Objective:
+## Objective:
 
-##Scripting to anonymize data collected by customers so that the data can be provided to Docker.
+## Scripting to anonymize data collected by customers so that the data can be provided to Docker.
 
-##Required outcomes:
+## Required outcomes:
 
 - Original data must not be changed.  We make a copy and modify that.
 - All changes are logged
 - Sane replacement so that obfuscated IP addresses, hostnames, etc can be logically followed in the logs
+
+
+## Usage :
+Edit the `dump_clean.sh` to change the variables at the top of the script. Specifically the URL, data_dir and password.
+
+Then Run...
+
+```
+./dump_clean.sh
+```
